2fcf644cdc80cb0315ff7d562cde24e58987c894
Cocona-Matsuda <skyebell13rei@gmail.com>
2021-05-14 14:05:27 +0900

    リスト一覧画面とだけ表示 debugのタグが気になる

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,39 @@ class MyTodoApp extends StatelessWidget{
     );
   }
 }
-
+//リスト一覧画面用
 class TodoListPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context){
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
       ),
     );
   }

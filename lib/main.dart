e92788c7bdf14cf4baf77cd692deef0c5c2e160d
Cocona-Matsuda <skyebell13rei@gmail.com>
2021-05-16 00:16:13 +0900

    データ受け渡し途中

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,24 @@ class TodoListPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('リスト一覧画面'),
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(title: Text('ニンジンを買う'),),
+          ),
+          Card(
+            child: ListTile(title: Text('カレールーを買う'),),
+          ),
+          Card(
+            child: ListTile(title: Text('タマネギを買う'),),
+          ),
+          Card(
+            child: ListTile(title: Text('ジャガイモを買う'),),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
@@ -40,16 +56,58 @@ class TodoListPage extends StatelessWidget{
 
 class TodoAddPage extends StatelessWidget{
   @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
+class _TodoAddPageState extends State<TodoPage>{
+  String _text = '';
+
+  @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: (){
-            Navigator.of(context).pop();
-          },
-          child: Text('リスト追加画面（クリックで戻る）'),
+      appBar: AppBar(
+        title: Text('リスト追加'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_text, style: TextStyle(color: Colors.blue)),
+            const SizedBox(height: 8),
+            TextField(
+              onChanged: (Stringvalue){
+                setState((){
+                  _text = value;
+                });
+
+                ),
+                const SizedBox(height: 8),
+                Container(
+                width: double.infinity,
+                color: Colors.blue,
+                child: TextButton(
+                onPressed: (){
+                Navigator.of(context).pop();
+                },
+                child: Text('リスト追加', style: TextStyle(color: Colors.white),),
+                ),
+                ),
+                const SizedBox(height
+              }: 8),
+            Container(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                child: Text('キャンセル'),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+  )
 }
